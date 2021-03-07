a47aeba614d7eebceca40e5bb6b24a22275d657f
RajatGupta02 <rajatgupta142002@gmail.com>
2021-03-07 15:46:20 +0530

    Codeforces bot script added

---
+++ hackathon.py
@@ -0,0 +1,101 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from PIL import Image
+from tabulate import tabulate
+from terminaltables import AsciiTable
+import time
+import os
+import linecache
+
+
+
+options= webdriver.ChromeOptions()
+options.headless=True
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH,options=options)
+#write code to login!!!!
+driver.get('https://codeforces.com/enter?back=%2F')
+username=driver.find_element_by_id('handleOrEmail')
+username.send_keys('rajatgupta142002@gmail.com')
+
+password=driver.find_element_by_id('password')
+password.send_keys('technokret21*')
+
+button=driver.find_element_by_class_name('submit')
+button.click()
+
+time.sleep(2)
+
+print("Please enter your command, type help for list of commands: ")
+command=input()
+
+
+if command=='upcoming' or command=='Upcoming':
+    driver.get("https://codeforces.com/contests?complete=true")
+    time.sleep(1)
+    name_list=[]
+    date_list=[]
+    
+    
+    
+    index=2
+    while True:
+        try:
+          status=driver.find_element_by_xpath(('//*[@id="pageContent"]/div[1]/div[1]/div[6]/table/tbody/tr[{}]/td[5]').format(index))
+          status_text=status.text
+          
+          if "Before" in status_text:
+              contest=driver.find_element_by_xpath(('//*[@id="pageContent"]/div[1]/div[1]/div[6]/table/tbody/tr[{}]/td[1]').format(index))
+              contest_name=contest.text  
+
+              date=driver.find_element_by_xpath(('//*[@id="pageContent"]/div[1]/div[1]/div[6]/table/tbody/tr[{}]/td[3]').format(index))
+              date_text=date.text
+              
+              name_list.append(contest_name)
+              date_list.append(date_text)
+              
+              
+          index+=1
+        except:
+          break    
+    titles = ['Name','Date']
+    data = [titles] + list(zip(name_list, date_list))
+
+    for i, d in enumerate(data):
+        line = '|'.join(str(x).ljust(89) for x in d)
+        print(line)
+        if i == 0:
+           print('-' * len(line))
+
+if command=='help' or command=='Help':
+    print("----------------------------------------------------------------------")
+    print("help           |        gives the list of commands")
+    print("----------------------------------------------------------------------")
+    print("upcoming       |        gives the list of upcoming CodeForces Contests")
+    print("----------------------------------------------------------------------")
+    print("rating         |        gives your current rating")
+    print("----------------------------------------------------------------------")
+
+
+if command=='rating' or command=='Rating':
+   print("Enter codeforces handle of user(It is case-sensitive): ")
+   userhandle=input()
+
+   driver.get('https://codeforces.com/api/user.rating?handle={}'.format(userhandle))
+   content=driver.find_element_by_tag_name('pre').text
+   
+   if content.rfind("newRating")!=-1:
+       bracket_index=content.rfind("}",0,-2)
+       print(content[content.rfind("newRating")+11:bracket_index])
+   elif "FAILED" in content:
+       print("USER NOT FOUND !! INVALID HANDLE")
+   else:
+       print("Unrated")        
+
+
+        
